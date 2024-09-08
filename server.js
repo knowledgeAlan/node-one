143d5655511ddb71d1b67b0780c54e8e01f8c79b
knowledgeAlan <1299076979@qq.com>
2024-09-08 20:30:57 +0800

    swagger

--- server.js
+++ server.js
@@ -1,11 +1,33 @@
 const express = require('express');
 const app = express();
-
+const swaggerUI = require("swagger-ui-express");
+const swaggerJsDoc = require("swagger-jsdoc");
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.set("view engine","ejs");
 
+
+const options = {
+	definition: {
+		openapi: "3.0.0",
+		info: {
+			title: "Library API",
+			version: "1.0.0",
+			description: "A simple Express Library API",
+		},
+		servers: [
+			{
+				url: "http://localhost:9000",
+			},
+		],
+	},
+	apis: ["./router/*.js"],
+};
+
+const specs = swaggerJsDoc(options);
+
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
 const userRouter = require("./router/users");
 
 app.use("/users", userRouter);
